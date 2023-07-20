feb5193790b75a6d7014d903d6e71fd770dfb620
Joshua Rose <joshuarose@gmx.com>
2023-07-20 13:10:06 +1000

    feat(tests): Add http tests, linting corrections
    * Add additional tests including fixtures for tests
    * Blacken code
    * Additional dependencies / requirements
    * Add tox file to gitignore as that is a client-sided file
    
    Authored-by: Joshua Rose <joshuarose099@gmail.com>

--- tests/test_host.py
+++ tests/test_host.py
@@ -2,10 +2,13 @@
 Test the hosting services that this application relies on.
 """
 
+import pytest
 import requests
 
+
+@pytest.mark.integration
 def test_extern_status():
-    """ Get hosting status of twitter vid """
+    """Get hosting status of twitter vid"""
 
     HOST = "https://twittervid.com"
     response = requests.head(HOST)
